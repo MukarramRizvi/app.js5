7e1b159c6eabbf0a4321c8c862eaed4e5e7bd5e4
MukarramRizvi <imranmaster@20gmail.com>
2024-03-29 22:31:10 +0500

    add two files

---
+++ app.js
@@ -0,0 +1,18 @@
+// var roundnumber = Math.round();
+// console.log(roundnumber);
+// var orignum = 5.3;
+// var num = Math.ceil(orignum);
+// console.log(num);
+// var orignum1 = 5.7;
+// var num1 = Math.floor(orignum1);
+// console.log(num1);
+// var orignum2 = 6.8;
+// var num2 = Math.round(orignum2);
+// console.log(num2);'
+// var roundednumber = Math.floor(0.5);
+// var ronudingnum = Math.random();
+// var scallernum = Math.floor(ronudingnum * 50) + 1;
+// console.log("random num in the range of 1 to 50",scallernum);
+var randomnum = Math.random(0,1);
+var randomnum1 = Math.floor(randomnum)
+console.log("Random Number",randomnum1)
